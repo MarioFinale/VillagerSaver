c0501007ac157896281e20e15facf9da00ed7a88
MarioFinale <mariofinale@hotmail.com>
2021-06-25 07:44:48 -0400

    Fix corrupted file

--- src/cl/mariofinale/VillagerSaver_Listener.java
+++ src/cl/mariofinale/VillagerSaver_Listener.java
@@ -15,85 +15,6 @@ public class VillagerSaver_Listener implements Listener{
 
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event){
-<<<<<<< HEAD
-        Entity villager = event.getEntity();
-        if(!(villager instanceof LivingEntity)) return;
-        LivingEntity tVillager = (LivingEntity)villager;
-        if (event.getDamager() == null) return;
-        Entity tKiller = event.getDamager();
-        if (!(tVillager.getHealth() - event.getDamage() <= 0)) return;
-        if (!(tVillager.getType() == EntityType.VILLAGER)) return;
-<<<<<<< HEAD
-<<<<<<< HEAD
-        if (!((tKiller.getType() == EntityType.ZOMBIE) || (tKiller.getType() == EntityType.ZOMBIE_VILLAGER)  || (tKiller.getType() == EntityType.DROWNED) || (tKiller.getType() == EntityType.HUSK))) return;
-        if (villagerSaver.WorldBlackList.contains(tVillager.getWorld().getName())) return;
-        StoreAndClearVillager(villager);
-        event.setCancelled(true);
-    }
-
-    public void StoreAndClearVillager(Entity villager){ //CALLED WHEN A VILLAGER IS ABOUT TO DIE
-        LivingEntity tVillager = (LivingEntity) villager;
-        Location tLoc = tVillager.getLocation();
-        World tWorld = tVillager.getWorld();
-        Ageable tAVillager = (Ageable) villager;
-        Villager oldVillager = (Villager) villager;
-        Entity vehicle = tVillager.getVehicle();
-        ZombieVillager zVillager;
-        if (tAVillager.isAdult()){
-            CraftVillager craftVillager = (CraftVillager) villager;
-            EntityVillager entityVillager = craftVillager.getHandle();
-            Reputation villagerReputations = entityVillager.fj();
-
-            Villager.Type villagerType = oldVillager.getVillagerType();
-            Villager.Profession villagerProfession = oldVillager.getProfession();
-            int villagerLevel = oldVillager.getVillagerLevel();
-            int villagerExp = oldVillager.getVillagerExperience();
-            List<MerchantRecipe> villagerTrades = new ArrayList<>(oldVillager.getRecipes());
-            Location villagerJobSite =  oldVillager.getMemory(MemoryKey.JOB_SITE);
-            Location villagerHome = oldVillager.getMemory(MemoryKey.HOME);
-
-            tVillager.setMemory(MemoryKey.JOB_SITE, new Location(tVillager.getWorld(), 0d,0d,0d));
-            tVillager.setMemory(MemoryKey.HOME, new Location(tVillager.getWorld(), 0d,0d,0d));
-            tVillager.remove();
-
-            Entity newZombie =  tWorld.spawnEntity(tLoc, EntityType.ZOMBIE_VILLAGER);
-            zVillager =  (ZombieVillager) newZombie;
-            zVillager.setVillagerProfession(villagerProfession);
-            zVillager.setAdult();
-            UUID zombieUUID = newZombie.getUniqueId();
-
-            villagerSaver.VillagersReputation.put(zombieUUID, villagerReputations);
-            villagerSaver.VillagersTypes.put(zombieUUID,villagerType);
-            villagerSaver.VillagersProfessions.put(zombieUUID,villagerProfession);
-            villagerSaver.VillagersLevels.put(zombieUUID, villagerLevel);
-            villagerSaver.VillagersExp.put(zombieUUID, villagerExp);
-            villagerSaver.VillagersTrades.put(zombieUUID, villagerTrades);
-            villagerSaver.VillagersJobSites.put(zombieUUID, villagerJobSite);
-            villagerSaver.VillagersHomes.put(zombieUUID, villagerHome);
-
-        }else{
-            tVillager.remove();
-            Entity newZombie =  tWorld.spawnEntity(tLoc, EntityType.ZOMBIE_VILLAGER); //Baby villagers never have any profession
-            zVillager =  (ZombieVillager) newZombie;
-            zVillager.setBaby();
-=======
-        if (!((tKiller.getType() == EntityType.ZOMBIE) || (tKiller.getType() == EntityType.ZOMBIE_VILLAGER)  || (tKiller.getType() == EntityType.DROWNED) || (tKiller.getType() == EntityType.HUSK) || (tKiller.getType() == EntityType.ZOMBIFIED_PIGLIN))) return;
-        if (villagerSaver.WorldBlackList.contains(tVillager.getWorld().getName())) return;
-        handleSpawnZombieVillager(tVillager);
-        event.setCancelled(true);
-    }
-
-=======
-        if (!((tKiller.getType() == EntityType.ZOMBIE) || (tKiller.getType() == EntityType.ZOMBIE_VILLAGER)  || (tKiller.getType() == EntityType.DROWNED) || (tKiller.getType() == EntityType.HUSK) || (tKiller.getType() == EntityType.ZOMBIFIED_PIGLIN))) return;
-        if (villagerSaver.WorldBlackList.contains(tVillager.getWorld().getName())) return;
-        handleSpawnZombieVillager(tVillager);
-        event.setCancelled(true);
-    }
-
->>>>>>> parent of 688c16c (Minor changes)
-    public void handleSpawnZombieVillager(LivingEntity entityliving) {
-        CraftVillager craftVillager = (CraftVillager) entityliving;
-=======
         Entity damagedEntity = event.getEntity();
         if(!(damagedEntity instanceof LivingEntity)) return;
         LivingEntity damagedVillager = (LivingEntity) damagedEntity;
@@ -109,7 +30,6 @@ public class VillagerSaver_Listener implements Listener{
 
     public void handleSpawnZombieVillager(LivingEntity livingEnt) {
         CraftVillager craftVillager = (CraftVillager) livingEnt;
->>>>>>> parent of cb5e316 (Revert "Merge branch 'pr/14'")
         Entity vehicle = craftVillager.getVehicle();
 
         EntityVillager entityvillager = craftVillager.getHandle();
@@ -125,21 +45,11 @@ public class VillagerSaver_Listener implements Listener{
                 TransformReason.INFECTION,
                 SpawnReason.INFECTION
         );
-<<<<<<< HEAD
-        if (entityZombieVillager == null) {
-            return;
-        }
-=======
         if (entityZombieVillager == null) return;
->>>>>>> parent of cb5e316 (Revert "Merge branch 'pr/14'")
         CraftVillagerZombie craftVillagerZombie = (CraftVillagerZombie) entityZombieVillager.getBukkitEntity();
 
         if (vehicle != null && !vehicle.getPassengers().contains(craftVillagerZombie)) {
             vehicle.addPassenger(craftVillagerZombie);
-<<<<<<< HEAD
->>>>>>> parent of 688c16c (Minor changes)
-=======
->>>>>>> parent of cb5e316 (Revert "Merge branch 'pr/14'")
         }
 
         entityZombieVillager.setVillagerData(villagerData);
